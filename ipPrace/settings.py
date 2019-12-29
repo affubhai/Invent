17d0dbf45cf7a2627cd2e88ebd0535e351e63f2a
Aftab Khan <aftabkhan2002@gmail.com>
2019-12-28 21:42:42 -0800

    withProcFile

--- ipPrace/settings.py
+++ ipPrace/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ipPrac.urls'
+ROOT_URLCONF = 'ipPrace.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ipPrac.wsgi.application'
+WSGI_APPLICATION = 'ipPrace.wsgi.application'
 
 
 # Database
