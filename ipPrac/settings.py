a6a0b7034c63cbdf5f8e5acc83c0c8b99aaca173
Aftab Khan <aftabkhan2002@gmail.com>
2019-12-28 22:22:40 -0800

    SETTING'

--- ipPrac/settings.py
+++ ipPrac/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hoqyatc#+73mtr3%^mngy+9x_qnyn1&0g3dl))9=-flgjz)37-'
+# SECRET_KEY = 'hoqyatc#+73mtr3%^mngy+9x_qnyn1&0g3dl))9=-flgjz)37-'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventinc.herokuapp.com']
 
 
 # Application definition
---
+++ ~/.bash_profile
@@ -0,0 +1,2 @@
+export SECRET_KEY = 'bd72715db2e948da2ba6490450be9d6def2e1d0adb78e4a1'
+export DEBUG_VALUE = 'True'
