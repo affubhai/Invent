622ebb1ed8bb1cee2bdee63107a493884e3a1478
Aftab Khan <aftabkhan2002@gmail.com>
2019-12-28 21:50:19 -0800

    ProcFile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn django_ipPrace.wsgi
+web: gunicorn django_ipPrac.wsgi
--- ipPrac/settings.py
+++ ipPrac/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ipPrace.urls'
+ROOT_URLCONF = 'ipPrac.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ipPrace.wsgi.application'
+WSGI_APPLICATION = 'ipPrac.wsgi.application'
 
 
 # Database
